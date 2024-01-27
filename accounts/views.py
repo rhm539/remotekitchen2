edee1fbdcac8e23d6eac246df7d3163a5883172a
Abdur RoB <rhm539@gmail.com>
2024-01-27 23:20:12 +0600

    ok

--- accounts/views.py
+++ accounts/views.py
@@ -7,8 +7,3 @@ from django.views import generic
 
 
 
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
